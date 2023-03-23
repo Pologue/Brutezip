b1a442ba75d1cd007259992ee7adba7124c3c725
Pologue <113284140+Pologue@users.noreply.github.com>
2023-03-23 23:31:12 +0800

    Add files via upload

--- pojie.py
+++ pojie.py
@@ -1,12 +1,19 @@
 import zipfile
 import itertools
-chars = 'etaonrishdlfcmugypwbvkjxqzETAONRISHDLFCMUGYPWBVKJXQZ1234567890-=_+[]|;:",./<>?`~!@#$%^&*()'
-global is_succeed, length
+from tqdm import tqdm
+
+chars_all = 'etaonrishdlfcmugypwbvkjxqzETAONRISHDLFCMUGYPWBVKJXQZ1234567890-=_+[]|;:",./<>?`~!@#$%^&*()\'\\\{\} '
+chars_small = 'etaonrishdlfcmugypwbvkjxqz'
+chars_small_nums = 'etaonrishdlfcmugypwbvkjxqz1234567890'
+nums = '1234567890'
+
+global is_succeed
 length = 1
 is_succeed = False
+
 def baoli():
     global length, is_succeed
-    passwds = itertools.product(chars,repeat=length)
+    passwds = itertools.product(chars_small_nums,repeat=length)
     for passwd in passwds:
         passwd = ''.join(passwd)
         try:
@@ -17,6 +24,21 @@ def baoli():
         except Exception:
             print("错误尝试:",passwd)
             is_succeed = False
+
+def zidian():
+#    lines = len(open(dic).readlines())
+#    open(dic).close()
+    with open(dic, 'r') as f:
+        data = f.readlines()
+        for item in tqdm(data):
+#            line = 0
+            passwd = item.strip()
+            try:
+                myzip.extractall(pwd=str.encode(passwd))
+                print("破解成功,密码为:",passwd)
+                return
+            except Exception:
+                pass
 
 mode = input("请选择破解模式: 1.暴力破解 2.字典破解\n")
 #暴力破解
@@ -33,3 +55,9 @@ if mode == "1":
 #字典破解
 elif mode == "2":
     zfile = input("请输入压缩包路径：")
+    try:
+        myzip = zipfile.ZipFile(zfile)
+    except FileNotFoundError:
+        print("路径输入错误！")
+    dic = input("请输入字典路径:")
+    zidian()
